d41b86eb6eeceac741782b71e65c4895a52d24fd
Dagfinn1962 <130903243+Dagfinn1962@users.noreply.github.com>
2023-05-23 23:43:43 +0200

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -8,7 +8,7 @@ $SD_SAMPLER = array('DDIM', 'DDPM', 'K_DPMPP_2M','K_DPMPP_2S_ANCESTRAL', 'K_DPM_
 define('INIT_IMAGE_MODE','IMAGE_STRENGTH');
 $SD_ENGINE_LIST = array('stable-diffusion-v1-5', 'stable-diffusion-512-v2-0', 'stable-diffusion-768-v2-0', 'stable-diffusion-depth-v2-0', 'stable-diffusion-512-v2-1', 'stable-diffusion-768-v2-1', 'stable-diffusion-xl-beta-v2-2-2');
 define('OPENAI_AUDIO_URL','https://api.openai.com/v1/audio/transcriptions');
-define('OPENAI_API_KEY','');//use your own api key
+define('OPENAI_API_KEY','sk-XPB7yWuYn9C6AmBQ8KkgX1H4wakX4VG7XuIJWnT3ePvYGfw3');//use your own api key
 define('OPENAI_AUDIO_MODEL', 'whisper-1');
 $SD_STYLE_PRESET = array('3d-model', 'analog-film', 'anime', 'cinematic', 'comic-book', 'digital-art', 'enhance', 'fantasy-art', 'isometric', 'line-art', 'low-poly', 'modeling-compound', 'neon-punk', 'origami', 'photographic', 'pixel-art', 'tile-texture');
 define('STABILITYAI_IMG_UPSCALE_URL', STABILITYAI_BASE_URL.'/v1/generation/esrgan-v1-x2plus/image-to-image/upscale');
